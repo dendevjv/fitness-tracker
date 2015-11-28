e976b7b06d86c3e9eed6d4b680cdb4365e58a340
dendevjv <dendevjv@yandex.ru>
2015-11-28 18:53:55 +0300

    First working HelloWorld Spring MVC example

---
+++ src/main/java/ru/dendevjv/controller/HelloController.java
@@ -0,0 +1,16 @@
+package ru.dendevjv.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(value="/greeting")
+    public String sayHello(Model model) {
+        
+        model.addAttribute("greeting", "Hello world (controller: HelloController)");
+        
+        return "hello";
+    }
+}
