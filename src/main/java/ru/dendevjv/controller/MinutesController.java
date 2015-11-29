94ce48dee2f9462fcadd37341d219f5aa8d8fe35
dendevjv <dendevjv@yandex.ru>
2015-11-29 17:30:22 +0300

    resolving static files

--- src/main/java/ru/dendevjv/controller/MinutesController.java
+++ src/main/java/ru/dendevjv/controller/MinutesController.java
@@ -14,4 +14,11 @@ public class MinutesController {
         
         return "addMinutes";
     }
+    
+//    @RequestMapping(value = "/addMoreMinutes")
+//    public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+//        System.out.println("exercising chain: " + exercise.getMinutes());
+//        
+//        return "addMinutes";
+//    }
 }
